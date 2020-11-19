8bb2858c0ff9a190dd676a2b1015044ad6be746e
wukuizong <2029253245@qq.com>
2020-11-20 01:04:32 +1030

    modified version 1

--- 早餐组合_Combination_of_breakfast/main_version_1.cpp
+++ 早餐组合_Combination_of_breakfast/main_version_1.cpp
@@ -1,17 +1,20 @@
+//KUIZONG WU 19/11/2020
+//Depending on the answer <https://leetcode-cn.com/problems/2vYnGI/solution/shuang-zhi-zhen-bian-li-liang-ge-zu-shu-by-ranlizz/>
+//this program has two pointers, one points to the begining of staple[], another points to the end of drinks[]
 class Solution {
 public:
     int breakfastNumber(vector<int>& staple, vector<int>& drinks, int x) {
-        int temp=0,j = 0, i = 0, count = 0;
+        int temp = 0, j = 0, count = 0;
+        int i = 0; //pointer of staples
         sort(staple.begin(),staple.end());
         sort(drinks.begin(),drinks.end());
-        int len = staple.size();
-        j = drinks.size()-1;
-        while ((i<len)&&(j>=0))
+        j = drinks.size()-1; // pointer of drinks[]
+        while ((i<staple.size())&&(j>=0))
         {
             if (staple[i]+drinks[j]<=x)
             {
-                count+= j+1;
-                count = count%1000000007;
+                count+= j+1; 
+                count = count%1000000007; // avoid huge result
                 i++;
             }
             else
